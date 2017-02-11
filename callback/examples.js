18922ce7dc6bcf4fae736963aae9870a80c24ce9
meed <meed2000@gmail.com>
2017-02-11 21:36:44 +0700

    Fixes logs in callback examples

--- callback/examples.js
+++ callback/examples.js
@@ -4,15 +4,15 @@ import fetch from './index.js';
 
 // Request by title
 createRequest("Breaking Bad", function (resp) {
-  console.log("Breaking Bad's response: " + JSON.stringify(resp, null, 2)
+  console.log("Breaking Bad's response: " + JSON.stringify(resp, null, 2))
 })
 
 // Request by Actor
 createRequest({'actor': 'Jim Carrey'}, function (resp) {
-  console.log("Jim Carrey's movies: " + JSON.stringify(resp, null, 2)
+  console.log("Jim Carrey's movies: " + JSON.stringify(resp, null, 2))
 })
 
 // Request by Director
 createRequest({'director': 'Tim Burton'}, function (resp) {
-  console.log("Tim Burton's movies: " + JSON.stringify(resp, null, 2)
+  console.log("Tim Burton's movies: " + JSON.stringify(resp, null, 2))
 })
